c85dc18806b1b0a66f0f1429153558f48338fe66
高凡 <evanhunt.g@gmail.com>
2017-07-20 17:31:56 +0800

    edit

--- localStoragePolyfill.js
+++ localStoragePolyfill.js
@@ -1,81 +1,83 @@
 // https://developer.mozilla.org/zh-CN/docs/Web/API/Storage/LocalStorage
 export default {
-    polyfill () {
+    polyfill() {
         if (!window.localStorage) {
-            Object.defineProperty(window, 'localStorage', new (function () {
-                let aKeys = []
-                let oStorage = {}
+            Object.defineProperty(window, 'localStorage', new (function localStorage() {
+                const aKeys = [];
+                const oStorage = {};
                 Object.defineProperty(oStorage, 'getItem', {
-                    value: function (sKey) {
-                        return sKey ? this[sKey] : null
+                    value(sKey) {
+                        return sKey ? this[sKey] : null;
                     },
                     writable: false,
                     configurable: false,
-                    enumerable: false
-                })
+                    enumerable: false,
+                });
                 Object.defineProperty(oStorage, 'key', {
-                    value: function (nKeyId) {
-                        return aKeys[nKeyId]
+                    value(nKeyId) {
+                        return aKeys[nKeyId];
                     },
                     writable: false,
                     configurable: false,
-                    enumerable: false
-                })
+                    enumerable: false,
+                });
                 Object.defineProperty(oStorage, 'setItem', {
-                    value: function (sKey, sValue) {
+                    value(sKey, sValue) {
                         if (!sKey) {
-                            return
+                            return;
                         }
-                        document.cookie = escape(sKey) + '=' + escape(sValue) + '; expires=Tue, 19 Jan 2038 03:14:07 GMT; path=/'
+                        document.cookie = `${escape(sKey)}=${escape(sValue)}; expires=Tue, 19 Jan 2038 03:14:07 GMT; path=/`;
                     },
                     writable: false,
                     configurable: false,
-                    enumerable: false
-                })
+                    enumerable: false,
+                });
                 Object.defineProperty(oStorage, 'length', {
-                    get: function () {
-                        return aKeys.length
+                    get() {
+                        return aKeys.length;
                     },
                     configurable: false,
-                    enumerable: false
-                })
+                    enumerable: false,
+                });
                 Object.defineProperty(oStorage, 'removeItem', {
-                    value: function (sKey) {
+                    value(sKey) {
                         if (!sKey) {
-                            return
+                            return;
                         }
-                        document.cookie = escape(sKey) + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/'
+                        document.cookie = `${escape(sKey)}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
                     },
                     writable: false,
                     configurable: false,
-                    enumerable: false
-                })
-                this.get = function () {
-                    var iThisIndx
-                    for (var sKey in oStorage) {
-                        iThisIndx = aKeys.indexOf(sKey)
+                    enumerable: false,
+                });
+                this.get = function get() {
+                    let iThisIndx = 0;
+                    /* eslint-disable */
+                    for (const sKey in oStorage) {
+                        iThisIndx = aKeys.indexOf(sKey);
                         if (iThisIndx === -1) {
-                            oStorage.setItem(sKey, oStorage[sKey])
+                            oStorage.setItem(sKey, oStorage[sKey]);
                         } else {
-                            aKeys.splice(iThisIndx, 1)
+                            aKeys.splice(iThisIndx, 1);
                         }
-                        delete oStorage[sKey]
+                        delete oStorage[sKey];
                     }
+                    /* eslint-disable */
                     for (aKeys; aKeys.length > 0; aKeys.splice(0, 1)) {
-                        oStorage.removeItem(aKeys[0])
+                        oStorage.removeItem(aKeys[0]);
                     }
-                    for (var aCouple, iKey, nIdx = 0, aCouples = document.cookie.split(/\s*;\s*/); nIdx < aCouples.length; nIdx++) {
-                        aCouple = aCouples[nIdx].split(/\s*=\s*/)
+                    for (let aCouple, iKey, nIdx = 0, aCouples = document.cookie.split(/\s*;\s*/); nIdx < aCouples.length; nIdx += 1) {
+                        aCouple = aCouples[nIdx].split(/\s*=\s*/);
                         if (aCouple.length > 1) {
-                            oStorage[iKey = unescape(aCouple[0])] = unescape(aCouple[1])
-                            aKeys.push(iKey)
+                            oStorage[iKey = unescape(aCouple[0])] = unescape(aCouple[1]);
+                            aKeys.push(iKey);
                         }
                     }
-                    return oStorage
-                }
-                this.configurable = false
-                this.enumerable = true
-            })())
+                    return oStorage;
+                };
+                this.configurable = false;
+                this.enumerable = true;
+            })());
         }
-    }
-}
+    },
+};
